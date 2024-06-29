61870cc65468b28c58467ed644da04febd342f29
atifraza158 <atifraza.brw@gmail.com>
2024-06-29 15:28:16 +0500

    Generic Classes topic added

--- app/src/main/java/com/example/first_application/Generics.kt
+++ app/src/main/java/com/example/first_application/Generics.kt
@@ -4,15 +4,14 @@ fun main() {
     println("Generics in Kotlin...")
     val obj = GenClass<String>("Atif Raza");
     val obj2 = GenClass<Int>(24);
-    val obj3 = GenClass(true);
-
-    checkType("Lorem ipsum");
+    val obj3 = GenClass("Bs Computer Science");
 }
 
 // Generic Class With constructor
 class GenClass<T>(value: T){
     init {
         println("The Value is: $value");
+        checkType<T>(value);
     }
 }
 
